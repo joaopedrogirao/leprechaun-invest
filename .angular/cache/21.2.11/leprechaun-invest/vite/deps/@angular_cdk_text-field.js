34519208c8292c32c98651326884d25005790b3d
João <joaopedrogirao10@gmail.com>
2026-05-17 17:11:57 -0300

    feat: criando pasta backend

---
+++ .angular/cache/21.2.11/leprechaun-invest/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,17 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-6FCEBGPW.js";
+import "./chunk-PMBXMTGQ.js";
+import "./chunk-Q4DRL7WK.js";
+import "./chunk-CXCYAHEV.js";
+import "./chunk-VDSAHJF2.js";
+import "./chunk-TQ2XYSIR.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
---
+++ backend/README.md
@@ -0,0 +1,1 @@
+# Backend - Leprechaun Invest
